6eb57bf6b22a3d5590acd28bf01daa941f0c58d5
dmorle <morled@uwindsor.ca>
2021-05-13 17:14:02 -0400

    github stuff + masm linking

--- tedlang/include/tedlang/util/string.h
+++ tedlang/include/tedlang/util/string.h
@@ -2,6 +2,6 @@
 #define TEDLANG_STRING_H
 
 // Does not copy null termination character
-size_t __cdecl te_strncpy(char* dst, char* src, size_t sz);
+extern size_t __cdecl te_strncpy(char* dst, char* src, size_t sz);
 
 #endif
